4de9377870468709e5b36247899ac7d3c54d3ad9
Graren <rennycol006@gmail.com>
2018-06-17 01:36:36 +0200

    Adapter

---
+++ Adapter/src/com/ogcg/adapter/Main.java
@@ -0,0 +1,16 @@
+package com.ogcg.adapter;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RoundHole roundHole = new RoundHole( 5 );
+        SquarePegAdapter squarePegAdapter;
+        for (int i = 6; i < 10; i++) {
+            squarePegAdapter = new SquarePegAdapter((double)i);
+            // The client uses (is coupled to) the new interface
+            squarePegAdapter.makeFit(roundHole);
+        }
+	}
+
+}
---
+++ Adapter/src/com/ogcg/adapter/SquarePeg.java
@@ -0,0 +1,17 @@
+package com.ogcg.adapter;
+
+class SquarePeg {
+    private double width;
+
+    public SquarePeg(double width) {
+        this.width = width;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+}
